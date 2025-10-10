eac1ccb80dc10d562bd38c838367362ae4d2442c
Havrylenko Olexii <hristinson@gmail.com>
2025-10-10 17:03:50 +0300

    fix:link

--- frontend/src/hooks/useItems.ts
+++ frontend/src/hooks/useItems.ts
@@ -7,8 +7,7 @@ type Item = {
   price: number;
 };
 
-// const URL = "https://teashop-n3kp.onrender.com/items"; https://corsfix.com/https://teashop-n3kp.onrender.com/items
-const URL = "https://corsfix.com/https://teashop-n3kp.onrender.com/items";
+const URL = "https://teashop-n3kp.onrender.com/items";
 
 export function useItems() {
   const [items, setItems] = useState<Item[]>([]);
